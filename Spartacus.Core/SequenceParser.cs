ba873d63b11d39b0c646c939d17815b08bf17fe5
Philip Laureano <philip.laureano@gmail.com>
2022-04-18 22:45:20 +1000

    Added the sequence parser

---
+++ Spartacus.Core/SequenceParser.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using Optional;
+using Optional.Unsafe;
+
+namespace Spartacus.Core;
+
+public class SequenceParser : IParser
+{
+    private readonly IEnumerable<IParser> _parsers;
+
+    public SequenceParser(params IParser[] parsers)
+    {
+        _parsers = parsers;
+    }
+
+    public async ValueTask<Option<ReadOnlyMemory<char>>> ParseAsync(ReadOnlyMemory<char> input)
+    {
+        if (!_parsers.Any())
+            return Option.None<ReadOnlyMemory<char>>();
+
+        var nextInput = input;
+        var parsedValues = new StringBuilder();
+        var numberOfCharsRead = 0;
+        foreach (var parser in _parsers)
+        {
+            var result = await parser.ParseAsync(nextInput);
+            if (!result.HasValue)
+                return Option.None<ReadOnlyMemory<char>>();
+
+            if (!result.HasValue) 
+                continue;
+            
+            var currentValue = result.ValueOrFailure();
+            parsedValues.Append(currentValue);
+
+            numberOfCharsRead += currentValue.Length;
+            nextInput = input[numberOfCharsRead..];
+        }
+        
+        var combinedValues = parsedValues.ToString();
+        return Option.Some(combinedValues.AsMemory());
+    }
+}
---
+++ Tests/SequenceParserTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Spartacus.Core;
+using Xunit;
+
+namespace Tests;
+
+public class SequenceParserTests
+{
+    [Fact(DisplayName = "The sequence parser should return the parsed sequence if all the sub parsers are successful")]
+    public async Task ShouldParseSuccessfullyIfTheInputMatchesTheGivenSequence()
+    {
+        var digitParser = new DigitParser();
+        var letterParser = new LetterParser();
+        var parser = new SequenceParser(digitParser, letterParser);
+
+        var inputText = "1a";
+        await parser.ParseAsync(inputText).ShouldBeSuccessful(inputText);
+    }
+
+    [Fact(DisplayName = "The sequence parser should return nothing if one of its parsers in the sequence fails")]
+    public async Task ShouldReturnNothingIfOneSubParserFails()
+    {
+        var digitParser = new DigitParser();
+        var letterParser = new LetterParser();
+        var parser = new SequenceParser(digitParser, letterParser);
+
+        var inputText = "2-";
+        await parser.ParseAsync(inputText).ShouldNotBeSuccessful();
+    }
+}
