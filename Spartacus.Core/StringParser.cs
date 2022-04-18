40cbd082a940ba04f2a724c937f8d3f5237980ea
Philip Laureano <philip.laureano@gmail.com>
2022-04-18 22:22:33 +1000

    Added the string parser

---
+++ Spartacus.Core/StringParser.cs
@@ -0,0 +1,30 @@
+﻿using Optional;
+
+namespace Spartacus.Core;
+
+public class StringParser : IParser
+{
+    private readonly string _phrase;
+
+    public StringParser(string phrase)
+    {
+        _phrase = phrase ?? throw new ArgumentNullException(nameof(phrase));
+    }
+
+    public async ValueTask<Option<ReadOnlyMemory<char>>> ParseAsync(ReadOnlyMemory<char> input)
+    {
+        var phraseLength = _phrase.Length;
+
+        // Skip the comparison if the string length doesn't match
+        if (phraseLength > input.Length)
+            return Option.None<ReadOnlyMemory<char>>();
+
+        var maxLength = input.Length <= phraseLength ? input.Length : phraseLength;
+        var slice = input.Slice(0, maxLength);
+        var inputText = slice.ToString();
+
+        return string.CompareOrdinal(_phrase, inputText) == 0
+            ? Option.Some(inputText.AsMemory())
+            : Option.None<ReadOnlyMemory<char>>();
+    }
+}
---
+++ Tests/StringParserTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading.Tasks;
+using Spartacus.Core;
+using Xunit;
+
+namespace Tests;
+
+public class StringParserTests
+{
+    [Fact(DisplayName = "The string parser should match when a given phrase is met")]
+    public async Task ShouldBeAbleToParseStringPhrase()
+    {
+        var phrase = Guid.NewGuid().ToString();
+        IParser parser = new StringParser(phrase);
+
+        await parser.ParseAsync(phrase).ShouldBeSuccessful(phrase);
+    }
+
+    [Fact(DisplayName = "The string parser should return nothing if the input text does not match the expected text")]
+    public async Task ShouldReturnNoneIfTheTextDoesNotMatch()
+    {
+        var expectedPhrase = Guid.NewGuid().ToString();
+        var invalidPhrase = Guid.NewGuid().ToString() + Guid.NewGuid().ToString();
+        
+        IParser parser = new StringParser(expectedPhrase);
+        await parser.ParseAsync(invalidPhrase).ShouldNotBeSuccessful();
+    }
+}
