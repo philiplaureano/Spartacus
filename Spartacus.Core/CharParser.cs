05792ef53fdabe11f82d22a4db7f7dbd816b3efd
Philip Laureano <philip.laureano@gmail.com>
2022-04-18 21:12:45 +1000

    Added the CharParser

---
+++ Spartacus.Core/CharParser.cs
@@ -0,0 +1,16 @@
+﻿namespace Spartacus.Core;
+
+public class CharParser : SingleCharParser
+{
+    private readonly char _targetChar;
+
+    public CharParser(char targetChar)
+    {
+        _targetChar = targetChar;
+    }
+
+    protected override bool IsMatch(char ch)
+    {
+        return ch == _targetChar;
+    }
+}
---
+++ Tests/CharParserTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using Spartacus.Core;
+using Xunit;
+
+namespace Tests;
+
+public class CharParserTests
+{
+    [Fact(DisplayName = "The char parser should be able to parse a given character")]
+    public async Task ShouldBeAbleToParseChar()
+    {
+        var targetChar = 'a';
+        var parser = new CharParser(targetChar);
+        var result = await parser.ParseAsync("a");
+        
+        Assert.True(result.HasValue);
+    }
+
+    [Fact(DisplayName = "The char parser should return nothing if the character does not match the input")]
+    public async Task ShouldReturnNoneIfTheCharacterDoesNotMatch()
+    {
+        var targetChar = 'a';
+        var parser = new CharParser(targetChar);
+        var result = await parser.ParseAsync("b");
+        
+        Assert.False(result.HasValue);
+    }
+
+    [Fact(DisplayName = "The char parser should return nothing if the input text is empty")]
+    public async Task ShouldReturnNoneIfTheInputIsEmpty()
+    {
+        var targetChar = 'a';
+        var parser = new CharParser(targetChar);
+        var result = await parser.ParseAsync(string.Empty);
+        
+        Assert.False(result.HasValue);
+    }
+}
