40884ea03548fffb688250b1770b04bc8ccc4cb8
Philip Laureano <philip.laureano@gmail.com>
2022-04-18 23:02:49 +1000

    Added the alternative parser

---
+++ Spartacus.Core/AlternativeParser.cs
@@ -0,0 +1,29 @@
+﻿using Optional;
+
+namespace Spartacus.Core;
+
+public class AlternativeParser : IParser
+{
+    private readonly IEnumerable<IParser> _parsers;
+
+    public AlternativeParser(params IParser[] parsers)
+    {
+        _parsers = parsers;
+    }
+
+    public async ValueTask<Option<ReadOnlyMemory<char>>> ParseAsync(ReadOnlyMemory<char> input)
+    {
+        if (!_parsers.Any())
+            return Option.None<ReadOnlyMemory<char>>();
+
+        foreach (var parser in _parsers)
+        {
+            // Only one rule needs to match in order to be successful
+            var result = await parser.ParseAsync(input);
+            if (result.HasValue)
+                return result;
+        }
+        
+        return Option.None<ReadOnlyMemory<char>>();
+    }
+}
---
+++ Tests/AlternativeParserTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Spartacus.Core;
+using Xunit;
+
+namespace Tests;
+
+public class AlternativeParserTests
+{
+    [Fact(DisplayName = "The alternative parser should return a value if one of the sub parsers is successful")]
+    public async Task ShouldParseSuccessfullyIfOneOfTheSubParsersSucceeds()
+    {
+        var fizz = new StringParser("fizz");
+        var buzz = new StringParser("buzz");
+
+        var parser = new AlternativeParser(fizz, buzz);
+        await parser.ParseAsync("buzz").ShouldBeSuccessful("buzz");
+        await parser.ParseAsync("fizz").ShouldBeSuccessful("fizz");
+    }
+
+    [Fact(DisplayName = "The alternative parser should return nothing if none of its sub parsers are successful")]
+    public async Task ShouldReturnNothingIfAllSubParsersFailToMatch()
+    {
+        var fizz = new StringParser("fizz");
+        var buzz = new StringParser("buzz");
+
+        var parser = new AlternativeParser(fizz, buzz);
+        await parser.ParseAsync("beep").ShouldNotBeSuccessful();
+    }
+}
