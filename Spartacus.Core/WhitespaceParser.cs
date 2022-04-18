3fe12801329d8f6e1b16ea208b8cd763b961e36a
Philip Laureano <philip.laureano@gmail.com>
2022-04-18 22:01:48 +1000

    Added the Whitespace parser

---
+++ Spartacus.Core/WhitespaceParser.cs
@@ -0,0 +1,9 @@
+﻿namespace Spartacus.Core;
+
+public class WhitespaceParser : SingleCharParser
+{
+    protected override bool IsMatch(char ch)
+    {
+        return char.IsWhiteSpace(ch);
+    }
+}
---
+++ Tests/WhitespaceParserTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Spartacus.Core;
+using Xunit;
+
+namespace Tests;
+
+public class WhitespaceParserTests
+{
+    [Fact(DisplayName = "The whitespace parser should parse white space characters")]
+    public async Task ShouldParseWhitespaceChars()
+    {
+        var whitespace = " ";
+        var parser = new WhitespaceParser();
+        await parser.ParseAsync(whitespace).ShouldBeSuccessful(" ");
+    }
+
+    [Fact(DisplayName = "The whitespace parser should not parse invalid input")]
+    public async Task ShouldNotParseInvalidInput()
+    {
+        var invalidInput = "X";
+        var parser = new WhitespaceParser();
+        await parser.ParseAsync(invalidInput).ShouldNotBeSuccessful();
+    }
+    
+    [Fact(DisplayName = "The whitespace parser should not parse empty strings")]
+    public async Task ShouldNotParseEmptyStrings()
+    {
+        var text = string.Empty;
+        var parser = new WhitespaceParser();
+        await parser.ParseAsync(text).ShouldNotBeSuccessful();
+    }
+}
