4046f21c340151d27661c2adf40150b8a77c2847
Philip Laureano <philip.laureano@gmail.com>
2022-04-18 21:12:59 +1000

    Fixed the warnings in the SingleCharParser class

--- Spartacus.Core/SingleCharParser.cs
+++ Spartacus.Core/SingleCharParser.cs
@@ -1,20 +1,18 @@
-﻿
-using Optional;
+﻿using Optional;
 
 namespace Spartacus.Core;
 
 public abstract class SingleCharParser : IParser
 {
-    public async ValueTask<Option<ReadOnlyMemory<char>>> ParseAsync(ReadOnlyMemory<char> input)
+    public ValueTask<Option<ReadOnlyMemory<char>>> ParseAsync(ReadOnlyMemory<char> input)
     {
         if (input.Length == 0)
-            return Option.None<ReadOnlyMemory<char>>();
+            return ValueTask.FromResult(Option.None<ReadOnlyMemory<char>>());
 
         var currentChar = input.ToArray()[0];
-        if (!IsMatch(currentChar))
-            return Option.None<ReadOnlyMemory<char>>();;
-
-        return Option.Some(new ReadOnlyMemory<char>(new char[] { currentChar }));
+        return ValueTask.FromResult(!IsMatch(currentChar)
+            ? Option.None<ReadOnlyMemory<char>>()
+            : Option.Some(new ReadOnlyMemory<char>(new char[] { currentChar })));
     }
 
     protected abstract bool IsMatch(char ch);
